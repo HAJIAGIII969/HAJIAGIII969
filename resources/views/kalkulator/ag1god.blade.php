65e73410f062ed70227d16e6c0e029176d784c4a
haggie auberta <pilotgaming231@gmail.com>
2024-02-22 13:56:16 +0700

    pertama

---
+++ resources/views/kalkulator/ag1god.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>KALKULATOR SADBOY</center>
+    <center><form name="form1" action="" method="post">
+        <input type="text" name="txt1" id="">
+
+<br>
+        <input type="button" value="1" onClick="form1.txt1.value='1'">  
+        <input type="button" value="2" onClick="form1.txt1.value='2'">
+        <input type="button" value="3" onClick="form1.txt1.value='3'">
+        <input type="button" value="+" onClick="form1.txt1.value='+'"> <br>
+        <input type="button" value="4" onClick="form1.txt1.value='4'">     
+        <input type="button" value="5" onClick="form1.txt1.value='5'">  
+        <input type="button" value="6" onClick="form1.txt1.value='6'">
+        <input type="button" value="-" onClick="form1.txt1.value='-'"> <br>
+        <input type="button" value="7" onClick="form1.txt1.value='7'">   
+        <input type="button" value="8" onClick="form1.txt1.value='8'">  
+        <input type="button" value="9" onClick="form1.txt1.value='9'">
+        <input type="button" value="/" onClick="form1.txt1.value='/'">  <br>
+        <input type="button" value="0" onClick="form1.txt1.value='0'">
+        <input type="button" value="<" onClick="form1.txt1.value='<'">  
+        <input type="button" value="tekan ini buat move on" onClick="form1.txt1.value='move on succes'">
+        <input type="button" value="x" onClick="form1.txt1.value='x'">
+       
+        </center>
+</body>
+</html>
